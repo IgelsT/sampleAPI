7d486299b5174c0902a439dc69097b9674821772
Igels <sergik_t@inbox.ru>
2024-10-07 12:27:30 +0300

    baseV1

--- Controllers/TestController.php
+++ Controllers/TestController.php
@@ -4,41 +4,10 @@ declare(strict_types=1);
 
 namespace Controllers;
 
-use DTO\TestDTO;
-use DTO\TestDTOData;
-
 class TestController
 {
     function index()
     {
-        $jsonStr = '{
-            "action": "login",
-            "data": {
-              "device_uid": "deviceUID12345678",
-              "username": "admin",
-              "pass": "md5pass1234567890",
-              "rowsInt": [
-                    {"id": 1, "name": "first" },
-                    {"id": 2, "name": "seecond" }
-              ]              
-            },
-            "rows": [
-                {"id": 1, "name": "first" },
-                {"id": 2, "name": "seecond" }
-            ],
-            "rowsIndexes": [100,101,102],
-            "isActive": true,
-            "startIdx": 17,
-            "price": 45.78
-          }';
-
-        $data = json_decode($jsonStr, true, 512, JSON_THROW_ON_ERROR);
-        $dto = new TestDTO($data);
-        print_r($dto);
-
-        $dtoData = new TestDTOData($data['data']);
-        print_r($dtoData);
-        // return ['dataFull' => $data, 'data' => $dtoData];
-        return ['OK'];
+        return ['OK' => 'OK'];
     }
 }
--- DTO/TestDTO.php
+++ DTO/TestDTO.php
@@ -7,45 +7,5 @@ use DTO\JSON\JSONAttribute;
 
 class TestDTO
 {
-    var string $action;
-
-    #[JSONAttribute(field: 'data')]
-    var TestDTOData $data;
-
-    #[JSONAttribute(field: 'rows', type: TestDTORows::class)]
-    var array $rows;
-
-    #[JSONAttribute(field: 'rowsIndexes')]
-    var array $rowsIdx;
-
-    var bool $isActive;
-
-    var int $startIdx;
-
-    var float $price;
-
-    var int $endIdx;
-
-    public function __construct(?array $data = null) {
-        Unmarshal::unmarshalArray($data, $this);
-    }
-}
-
-class TestDTOData
-{
-    var string $device_uid;
-    var string $username;
-    var string $pass;
-    #[JSONAttribute(field: 'rowsInt', type: TestDTORows::class)]
-    var array $rows;
-
-    public function __construct(?array $data = null) {
-        Unmarshal::unmarshalArray($data, $this);
-    }
-}
-
-class TestDTORows
-{
-    var int $id;
-    var string $name;
+    var string $myValue;
 }
